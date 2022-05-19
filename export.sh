70a3c45eed72e6c290ffa96f678229fd13c7b6d6
Mark Q Schwanda <mqschwanda@gmail.com>
2022-05-19 04:47:42 -0400

    chore(naming): update variable names and default directories

--- export.sh
+++ export.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 _os="`uname`"
 _now=$(date +"%m_%d_%Y")
-_file="wp-data/data_$_now.sql"
+_file="wordpress/data/data_$_now.sql"
 
 # Export dump
 EXPORT_COMMAND='exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"'
